851f9355e54785f9a61cf0b6e4f6f7174fdd394a
atommnava <atommunoznava@gmail.com>
2025-05-24 13:22:24 -0600

    MYSQL Access

--- database.sql
+++ database.sql
@@ -1,5 +1,6 @@
 CREATE DATABASE CRYPTO;
 USE CRYPTO;
+
 CREATE TABLE blockchain(
     number varchar(16),
     hash varchar(64),
@@ -7,3 +8,11 @@ CREATE TABLE blockchain(
     data varchar(128),
     nonce varchar(16)
 );
+
+CREATE TABLE users(
+     name VARCHAR(64),
+     username VARCHAR(32),
+     email VARCHAR(64),
+     password VARCHAR(128)
+);
+
