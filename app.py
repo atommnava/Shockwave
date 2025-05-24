dec799dff38d56e6eb5a23cdd17937fc5f795605
atommnava <atommunoznava@gmail.com>
2025-05-24 09:06:40 -0600

    Flask Implementation

---
+++ app.py
@@ -0,0 +1,26 @@
+#!urs/bin/python
+# -*- coding: utf-8 -*-
+
+from flask import Flask, render_template, flash, redirect, url_for, session, request, logging
+from passlib.hash import sha256_crypt
+from flask_mysqldb import MySQL
+
+app = Flask(__name__)
+
+# Configuration for all our setting from MySQL
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USERNAME'] = 'root'
+app.config['MYSQL_PASSWORD'] = ''
+app.config['MYSQL_DATABASE'] = 'CRYPTO'
+app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
+
+mysql = MySQL(app)
+
+@app.route("/")
+def index():
+    return "Hello, Atom!"
+
+if __name__ == "__main__":
+    app.secret_key = 'secret256'
+    app.run(debug=True)
+
