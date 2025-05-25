a217af0bbcf817283f45d2c2287eff7083b9c626
atommnava <atommunoznava@gmail.com>
2025-05-24 18:52:48 -0600

    MYSQL Access

--- sqlhelpers.py
+++ sqlhelpers.py
@@ -4,25 +4,41 @@ class Table():
     def __init__(self, tableName, *args):
         self.table = tableName
         self.columns = "(%s)" %",".join(args)
-        if isNewTable(tableName):
+        if isnewtable(tableName):
             cur = mysql.connection.cursor()
             cur.execute("CREATE TABLE %s%s" %(self.table, self.columns))
             cur.close()
 
     def getAll(self):
-        pass
+        cur = mysql.connection.cursor()
+        result = cur.execute("SELECT * FROM %s" %self.table)
+        data = cur.fetchAll(); return data
 
-    def getOne(self):
-        pass
+    def getOne(self, search, value):
+        data = {}
+        cur = mysql.connection.cursor()
+        result = cur.execute("SELECT * FROM %s WHERE %s = \"%s\""  %(self.table, search, value))
+        if result > 0: data = cur.fetchone()
+        cur.close(); return data
 
     def deleteOne(self, search, value):
-        pass
+        cur = mysql.connection.cursor()
+        cur.execute("DELETE FROM %s WHERE %s = \"%s\"" %(self.table, search, value))
+        mysql.connection.commit(); cur.close
 
     def drop(self):
-        pass
+        cur = mysql.connection.cursor()
+        cur.execute("DROP TABLE %s" %(self.table))
+        cur.close()
 
     def insert(self, *args):
-        pass
+        cur = mysql.connection.cursor()
+        data = ""
+        for arg in args:
+            data += "\"%s\"," %(arg)
+        cur.execute("INSERT INTO %s%s VALUES(%s)" %(self.table, self.columns, data[:len(data) -1]))
+        mysql.connection.commit()
+        cur.close()
 
     #check if table already exists
 def isnewtable(tableName):
