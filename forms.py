ef4f9547a65d7c276c31a4e813869408c0ac7407
atommnava <atommunoznava@gmail.com>
2025-05-26 16:28:57 -0600

    Adding Forms

--- forms.py
+++ forms.py
@@ -1,4 +1,8 @@
 from wtforms import Form, StringField, DecimalField, IntegerField, TextAreaField, PasswordField, validators
 
 class RegisterForm(Form):
-    name = StringField('Full Name', [validators.Length])
+    name = StringField('Full Name', [validators.Length(min=1, max=64)])
+    username = StringField('Username', [validators.Length(min=1, max=16)])
+    email = StringField('Email', [validators.Length(min=6, max=64)])
+    password = PasswordField('Password', [validators.DataRequired(), validators.EqualTo('confirm', message="Password do not match")])
+    confirm = PasswordField('Confirm Password')
